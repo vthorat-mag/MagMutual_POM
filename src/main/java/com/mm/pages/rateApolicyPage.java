8f8e3096625d19725649fd1b49993a367d263cc9
vthorat <vthorat@TSMMICDEVTEST.magnt.magmutual.com>
2018-05-04 04:41:45 -0400

    FrameWork Updates.

--- src/main/java/com/mm/pages/rateApolicyPage.java
+++ src/main/java/com/mm/pages/rateApolicyPage.java
@@ -1,5 +1,139 @@
 package com.mm.pages;
 
-public class rateApolicyPage {
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.mm.utils.commonMethods;
+import com.mm.utils.commonUtilities;
+
+public class rateApolicyPage extends commonMethods {
+	
+	WebDriver driver;
+	
+	@FindBy(name="globalSearch")
+	WebElement Policy_Search;
+
+	@FindBy(name="search")
+	WebElement Search_btn;
+
+	@FindBy(id="PM_COMMON_TABS_RATE")
+	WebElement RateBtn;
+
+	@FindBy(name="483865737.confirmed")
+	WebElement Continue_saving;
+
+	@FindBy(id="PM_NOTIFY_CLOSE")
+	WebElement Notify_Close;
+
+	@FindBy(id="PM_VIEW_PREM_CLOSE")
+	WebElement Prem_Close;
+
+	@FindBy(name="btnSaveAsCSV")
+	WebElement Export;
+
+	@FindBy(name="workflowExit_Ok")
+	WebElement Exit_Ok;
+	
+	public rateApolicyPage(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	public void policySearch() throws InterruptedException, AWTException{
+		
+		Thread.sleep(3000);
+		clearTextBox(Policy_Search);
+		enterTextIn(Policy_Search, "Q09101299-NB18-01");
+		click(Search_btn);
+		click(RateBtn);
+		Thread.sleep(5000);
+	}
+	public void saveRatedetails() throws InterruptedException
+	{
+		switchToFrame(driver,"popupframe1");
+	    Thread.sleep(1000);
+	    selectDropdown(Continue_saving, "Y"); 
+	    Thread.sleep(1000);
+	    click(Notify_Close);
+	    Thread.sleep(3000);
+	}
+	
+				
+	public void startExcelExport() throws InterruptedException,AWTException
+	{
+	    	 click(Export);
+	    	 Thread.sleep(2000);
+	    	 
+	    	 Robot rob = new Robot();
+	    	 
+	    	 rob.keyPress(KeyEvent.VK_F6);
+	    	 rob.keyRelease(KeyEvent.VK_F6);
+	    	 
+	    	 rob.setAutoDelay(1000);
+	    	 rob.keyPress(KeyEvent.VK_TAB);
+	    	 rob.keyRelease(KeyEvent.VK_TAB);
+	    	 
+	    	
+	    	 rob.setAutoDelay(1000);
+	    	 rob.keyPress(KeyEvent.VK_DOWN);
+	    	 
+	    	 rob.setAutoDelay(1000);
+	    	 rob.keyPress(KeyEvent.VK_DOWN);
+	    	 rob.keyRelease(KeyEvent.VK_DOWN);
+	    	 
+	    	 
+	    	 rob.setAutoDelay(1000);
+	    	 rob.keyPress(KeyEvent.VK_ENTER);
+	    	 rob.keyRelease(KeyEvent.VK_ENTER);
+	    	 
+	    	 DateFormat dateFormatter = new SimpleDateFormat("MMddyy_hhmmss");
+	 		 Date today = Calendar.getInstance().getTime();        
+	 		 String date= dateFormatter.format(today);
+	 		
+	 		 StringSelection sysDate = new StringSelection("C:\\MM_Testcase_Output\\"+date+".xlsx");
+	 		
+	 	 	 Toolkit.getDefaultToolkit().getSystemClipboard().setContents(sysDate, null);
+
+	 	 	 rob.keyPress(KeyEvent.VK_CONTROL); 									    //press 'cntrl' key
+			 rob.keyPress(KeyEvent.VK_V);											//press 'V' key
+			
+			 rob.keyRelease(KeyEvent.VK_CONTROL);
+			 rob.keyRelease(KeyEvent.VK_V);
+	 		 rob.setAutoDelay(2000);
+	    	 rob.keyPress(KeyEvent.VK_ENTER);
+	    	 rob.keyRelease(KeyEvent.VK_ENTER);
+	    	 
+	    	 click(Prem_Close);
+	    	 Thread.sleep(1000);
+	    	 
+	    	 click(Exit_Ok);
+	    	 switchToParentWindowfromframe(driver);
+	    	
+	    	 rob.keyPress(KeyEvent.VK_F6);
+	    	 rob.keyRelease(KeyEvent.VK_F6);
+	    	 rob.setAutoDelay(1000);
+	    	 rob.keyPress(KeyEvent.VK_TAB);
+	    	 rob.keyPress(KeyEvent.VK_TAB);
+	    	 rob.keyPress(KeyEvent.VK_TAB);
+	    	 rob.keyRelease(KeyEvent.VK_TAB);
+	    	 rob.setAutoDelay(1000);
+	    	 rob.keyPress(KeyEvent.VK_ENTER);
+	    	 rob.keyRelease(KeyEvent.VK_ENTER);
+	}
+
 
 }
---
+++ src/main/java/com/mm/utils/commonMethods.java
@@ -0,0 +1,125 @@
+package com.mm.utils;
+
+import java.util.Set;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import BaseClass.CommonActionInterface;
+
+public class commonMethods implements CommonActionInterface {
+
+
+	public void switchToParentWindowfromotherwindow(WebDriver driver, String parentwindow)
+	{
+		driver.switchTo().window(parentwindow);
+	}
+	
+	public void switchToFrame(WebDriver driver, String frameName)
+	{
+		
+		driver.switchTo().frame(frameName);
+	}
+	public void switchToParentWindowfromframe(WebDriver driver)
+	{
+		driver.switchTo().defaultContent();
+	}
+	
+	public String randomNoGenerator()
+	{
+		return RandomStringUtils.random(2,"1234567890");
+	}
+
+	public void navigateTo(String url) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void enterTextIn(WebElement pageElement, String text) {
+		// TODO Auto-generated method stub
+		pageElement.sendKeys(text);
+		
+	}
+
+	public void clickButton(WebElement pageElement) {
+		// TODO Auto-generated method stub
+		pageElement.click();
+		
+	}
+
+	public void waitFor(long ms) {
+		// TODO Auto-generated method stub
+	}
+
+	public void close(WebDriver driver) {
+		// TODO Auto-generated method stub
+		driver.quit();
+		
+	}
+
+	public void takeScreenShot(String pageTitle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getPageTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getText(WebElement pageElement) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void clearTextBox(WebElement pageElement) {
+		// TODO Auto-generated method stub
+		pageElement.clear();
+		
+	}
+
+	public String getAttributeValue(WebElement pageElement, String attributeName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void click(WebElement pageElement) {
+		// TODO Auto-generated method stub
+		pageElement.click();
+		
+	}
+
+	public void clear(WebElement pageElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void visibilityOfElement(WebElement pageElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String switchToWindow(WebDriver driver) {
+		Set<String> handles = driver.getWindowHandles();      //Return a set of window handle
+		 
+	    String parentWindow = driver.getWindowHandle();
+	   
+		 for(String currentWindow : handles){
+	    	   if(!currentWindow.equals(parentWindow))
+	    	  {
+	    	   driver.switchTo().window(currentWindow);
+	    	  }
+		 }
+		 return parentWindow;
+	}
+
+	public void selectDropdown(WebElement element, String DropDownOption) {
+		
+		Select Sel = new Select(element);
+		Sel.selectByValue(DropDownOption);
+	}
+
+
+}
