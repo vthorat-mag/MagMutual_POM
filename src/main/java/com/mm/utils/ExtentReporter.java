5ef60b3243b0513a3a9aba1416a45979502c7f64
vthorat <vthorat@TSMMICDEVTEST.magnt.magmutual.com>
2018-05-10 04:40:23 -0400

    POM.xml change rateApolicy page changes loginPage changes extent report class addition common utilities changes cisPage changes

---
+++ src/main/java/com/mm/utils/ExtentReporter.java
@@ -0,0 +1,10 @@
+package com.mm.utils;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+public class ExtentReporter {
+	
+	public static ExtentTest logger;
+	public static ExtentReports report= new ExtentReports(System.getProperty("user.dir")+"\\target\\Reports\\SmokeTestReport.HTML");
+}
