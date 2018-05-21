92185fe12e4a47f020dbccb52d8d565d57b5ea9f
vthorat <vthorat@TSMMICDEVTEST.magnt.magmutual.com>
2018-05-21 05:12:04 -0400

    Binder TC changes

--- src/main/java/com/mm/pages/rateApolicyPage.java
+++ src/main/java/com/mm/pages/rateApolicyPage.java
@@ -9,18 +9,30 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
+import com.mm.utils.ExtentReporter;
 import com.mm.utils.commonAction;
 import com.mm.utils.commonUtilities;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class rateApolicyPage extends commonAction {
 	
 	WebDriver driver;
+	String valueOfPolicyActionAccept = "javascript:acceptQuote();";
+	String billingSetup = "javascript:billingSetup();";
+	String paymentPlanValue = "660262192";
+	//Object repository for all elements on rate A policy page.
 	
 	@FindBy(name="globalSearch")
 	WebElement Policy_Search;
@@ -46,38 +58,89 @@ public class rateApolicyPage extends commonAction {
 	@FindBy(name="workflowExit_Ok")
 	WebElement Exit_Ok;
 	
+	@FindBy(xpath="//div[@id='globalDropdownActionItems']//select[@class='globalActionItemList']")
+	WebElement policyAction;
+	
+	//@FindBy(id="polPhaseCodeROSPAN")
+	@FindBy(xpath="//table[@id='formFieldsTableForHeaderFieldsSecond']//span[@id='polPhaseCodeROSPAN']")
+	WebElement policyPhaseBinder;
+	@FindBy(xpath="//select[@name='paymentPlanId']")
+	WebElement paymentPlan;
+	
+	@FindBy(xpath="//div[@class='horizontalButtonCollection']//input[@id='PM_BILLNG_SAVE']")
+	WebElement billingSetupSaveBtn;
+	
+	@FindBy(xpath="//a[@id='PM_PT_VIEWCVG']//span[@class='tabWithNoDropDownImage']")
+	WebElement coverageTab;
+	
+	@FindBy(xpath = "//table[@id='coverageListGrid']//tbody//td//div[@id='CPRODUCTCOVERAGEDESC']")
+	List<WebElement> coverageList;
+	
+	@FindBy(xpath = "//div[@class='horizontalButtonCollection']//span[@class='leftGrayButtonArea']//input[@id='PM_MANU_PUP']")
+	WebElement optionalFormBtn;
+	
+	@FindBy(xpath = "//table[@id='maintainManuscriptListGrid']")
+	WebElement 	manuscriptList;
+	
+	@FindBy(id="PM_MANU_DELETE)")
+	WebElement manuscriptPageDeleteBtn;
+	
+	@FindBy(id="PM_MANU_ADD)")
+	WebElement manuscriptPageAddBtn;
+	
+	@FindBy(xpath="//table[@id='selectManuscriptGrid']//tr[@class='hiliteSelectRow']//div[@id='CSHORTDESCRIPTION']")
+	List<WebElement> manuscriptAddListformName;
+	
+	@FindBy(xpath="//table[@id='selectManuscriptGrid']//tr[@class='hiliteSelectRow']//input[@name='chkCSELECT_IND']")
+	List<WebElement> manuscriptAddListformNameChkBox;
+	
+	@FindBy(id="PM_SEL_MANU_DONE")
+	WebElement manuscriptAddListDoneBtn;
+	
+	@FindBy(id="PM_MANU_SAVE")
+	WebElement manuscriptPageSaveBtn;
+	
+	@FindBy(id="PM_MANU_CLOSE")
+	WebElement manuscriptPageCloseBtn;
+	
+	@FindBy(xpath="//iframe[contains(@id='popupframe')]")
+	List<WebElement> allIframes;
+	
+	//Constructor to initialize all elements on this page.
 	public rateApolicyPage(WebDriver driver)
 	{
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
 	}
 	
-	
-	public void policySearch() throws InterruptedException, AWTException{
+	//Policy Search Code.
+	public void policySearch(String policyNo) throws InterruptedException, AWTException{
 		
 		Thread.sleep(3000);
-		clearTextBox(Policy_Search);
-		enterTextIn(Policy_Search, "Q09101299-NB18-01");
-		click(Search_btn);
-		click(RateBtn);
-		Thread.sleep(5000);
+		clearTextBox(Policy_Search, "Policy text field");
+		enterTextIn(Policy_Search, policyNo, "Policy text field");
+		click(Search_btn, "Search button");
 	}
+	
+	//Save Rate details code.
 	public void saveRatedetails() throws InterruptedException
 	{
+		click(RateBtn, "Rate button");
+		Thread.sleep(5000);
 		switchToFrame(driver,"popupframe1");
 	    Thread.sleep(1000);
-	    selectDropdown(Continue_saving, "Y"); 
+	    selectDropdown(Continue_saving, "Y", "Continue saving without Quote"); 
 	    Thread.sleep(1000);
-	    click(Notify_Close);
+	    click(Notify_Close, "Close button");
 	    Thread.sleep(3000);
 	}
 	
-				
+	//Donwload Excel report and save in defined folder
 	public void startExcelExport() throws InterruptedException,AWTException
 	{
-	    	 click(Export);
-	    	 Thread.sleep(2000);
-	    	 
+		   	 click(Export, "Export link");
+		   	 Thread.sleep(2000);
+			    	 
 	    	 Robot rob = new Robot();
 	    	 
 	    	 rob.keyPress(KeyEvent.VK_F6);
@@ -117,10 +180,10 @@ public class rateApolicyPage extends commonAction {
 	    	 rob.keyPress(KeyEvent.VK_ENTER);
 	    	 rob.keyRelease(KeyEvent.VK_ENTER);
 	    	 
-	    	 click(Prem_Close);
+	    	 click(Prem_Close, " premium view");
 	    	 Thread.sleep(1000);
 	    	 
-	    	 click(Exit_Ok);
+	    	 click(Exit_Ok, "Ok[Exit] Button");
 	    	 switchToParentWindowfromframe(driver);
 	    	
 	    	 rob.keyPress(KeyEvent.VK_F6);
@@ -134,6 +197,95 @@ public class rateApolicyPage extends commonAction {
 	    	 rob.keyPress(KeyEvent.VK_ENTER);
 	    	 rob.keyRelease(KeyEvent.VK_ENTER);
 	}
-
-
+	
+	public void AcceptFromActionDropDown()
+	{
+		selectDropdown(policyAction, valueOfPolicyActionAccept, "Policy Action");
+		
+	}
+	public void isAlertPresent() throws InterruptedException 
+	{ 
+	    try 
+	    { 
+	    	Thread.sleep(5000);
+	        driver.switchTo().alert().accept();
+	        //ExtentReporter.logger.log(LogStatus.INFO, alert.getText());
+	        /*driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	        alert.dismiss();*/
+	        Thread.sleep(5000);
+	    }   // try 
+	    catch (NoAlertPresentException Ex) 
+	    { 
+	    	ExtentReporter.logger.log(LogStatus.INFO, "Alert is not displayed for Same policy exist.");
+	    }   // catch 
+	}   
+	
+	public void identifyPhase() throws InterruptedException
+	{
+		waitFor(driver, 2);
+		String getTextPolicyPhase = policyPhaseBinder.getAttribute("innerText");
+		verifyTextPresent(getTextPolicyPhase,"Binder","Policy Phase");
+	}
+	public void billingSetup() throws InterruptedException
+	{
+		selectDropdown(policyAction, billingSetup, "Policy Action");
+		waitFor(driver, 5000);
+		switchToFrame(driver, "popupframe1");
+		selectDropdown(paymentPlan, paymentPlanValue, "Payment Plan");
+		Thread.sleep(5000);
+		//clickButton(billingSetupSaveBtn, "Save Button");
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click();", billingSetupSaveBtn);
+		Thread.sleep(35000);
+		switchToParentWindowfromframe(driver);
+	}
+	public void coverageDetailsSelect()
+	{
+		//click(coverageTab, "Coverage");
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click();", coverageTab);
+	}
+	
+	public void coverageUpdates(String CoverageName, String binderForm) throws InterruptedException
+	{
+		for (int i = 0; i<coverageList.size();i++)
+		{
+			if(coverageList.get(i).getAttribute("innerHTML").equals(CoverageName))
+			{
+				//click(coverageList.get(i),coverageList.get(i).getAttribute("innerHTML"));
+				Thread.sleep(4000);
+				JavascriptExecutor js = (JavascriptExecutor)driver;
+				js.executeScript("arguments[0].click();", coverageList.get(i));
+				
+				break;
+			}
+		}
+		//clickButton(optionalFormBtn, "Optional Form");
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click();", optionalFormBtn);
+		
+		for (int i=0;i<allIframes.size();i++)
+		{
+			System.out.println(allIframes.get(i).getAttribute("id"));
+		}
+		switchToFrame(driver, "popupframe7");
+		if (manuscriptList.isDisplayed())
+		{
+			click(manuscriptPageDeleteBtn, "Delete");
+			click(manuscriptPageAddBtn, "Add");
+		}else{
+			click(manuscriptPageAddBtn, "Add");
+		}
+		
+		for(int i=0;i<manuscriptAddListformName.size();i++)
+		{
+			if(manuscriptAddListformName.get(i).getAttribute("innerHTML").equals(binderForm))
+			{
+				click(manuscriptAddListformNameChkBox.get(i), "check Box for "+binderForm);
+				break;
+			}
+		}
+		
+		click(manuscriptAddListDoneBtn,"Done");
+	}
 }
--- src/test/java/MMTestCase/SmokeTestCase.java
+++ src/test/java/MMTestCase/SmokeTestCase.java
@@ -1,22 +1,29 @@
 package MMTestCase;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import java.awt.AWTException;
-
+import java.io.File;
+import java.io.IOException;
+import javax.net.ssl.SSLEngineResult.Status;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import com.mm.browsers.BrowserTypes;
 import com.mm.listeners.testListeners;
 import com.mm.pages.cisPage;
+import com.mm.pages.findPolicyPage;
 import com.mm.pages.homePage;
 import com.mm.pages.loginPage;
 import com.mm.pages.rateApolicyPage;
+import com.mm.utils.ExtentReporter;
+import com.mm.utils.commonUtilities;
+import com.relevantcodes.extentreports.LogStatus;
+
 
 public class SmokeTestCase extends BrowserTypes {
 	
@@ -25,20 +32,23 @@ public class SmokeTestCase extends BrowserTypes {
 	cisPage cispage;
 	homePage homepage;
 	rateApolicyPage rateapolicypage;
+	findPolicyPage findpolicypage;
 	
-	@BeforeTest	
+	@BeforeMethod
 	public void loginToeOasis() throws Exception
 	{
-		loginpage = new loginPage(driver);
-		loginpage.loginToeOasis();
 		testListeners testlist = new testListeners();
 	}
 	
-	@Test(testName ="Verify Add Organization")
+	//@Test(description="Verify Add Organization")
 	public void TC42404() throws Exception
 	{
+		ExtentReporter.logger=ExtentReporter.report.startTest("TC42404");
+		loginpage = new loginPage(driver);
+		loginpage.loginToeOasis();
+		ExtentReporter.logger.log(LogStatus.INFO, "User logged into application sucessfully.");
+		ExtentReporter.logger.log(LogStatus.INFO, "Started Add org  test");
 		homepage = new homePage(driver);
-	
 		homepage.navigateToCISPage();
 		cispage = new cisPage(driver);
 		cispage.clickOnNewOrganization();
@@ -47,26 +57,71 @@ public class SmokeTestCase extends BrowserTypes {
 		cispage.saveNewOrgDetails();
 	}
 	
-	@Test(testName="Hospital Rate")
-	public void TC42239() throws InterruptedException, AWTException
+	//@Test(description="Hospital Rate")
+	public void TC42239() throws Exception
 	{
+		ExtentReporter.logger=ExtentReporter.report.startTest("TC42239");
+		loginpage = new loginPage(driver);
+		loginpage.loginToeOasis();
+		ExtentReporter.logger.log(LogStatus.INFO, "User logged into application sucessfully.");
+		ExtentReporter.logger.log(LogStatus.INFO, "Started Add rate  test");
 		homepage = new homePage(driver);
 		homepage.navigateToPolicyPage();
 		rateapolicypage = new rateApolicyPage(driver);
-		rateapolicypage.policySearch();
+		//rateapolicypage.policySearch();
 		rateapolicypage.saveRatedetails();
 		rateapolicypage.startExcelExport();
 	}
 	
-	@AfterTest
-	public void logoffFromAppclication()
+	@Test(description="HPL - Binder")
+	public void TC42541() throws Exception
 	{
-		homepage.logoutFromeOasis();
+		ExtentReporter.logger=ExtentReporter.report.startTest("TC42541");
+		loginpage = new loginPage(driver);
+		loginpage.loginToeOasis();
+		ExtentReporter.logger.log(LogStatus.INFO, "User logged into application sucessfully.");
+		ExtentReporter.logger.log(LogStatus.INFO, "Started HPL - Binder  test");
+		homepage = new homePage(driver);
+		homepage.navigateToPolicyPage();
+		findpolicypage = new findPolicyPage(driver);
+		String policyNo = findpolicypage.findQuotewithActiveState();
+		rateapolicypage = new rateApolicyPage(driver);
+		rateapolicypage.policySearch(policyNo);
+		rateapolicypage.AcceptFromActionDropDown();
+		rateapolicypage.isAlertPresent();
+		ExtentReporter.logger.log(LogStatus.INFO, "Make sure Phase is set to Binder.");
+		//rateapolicypage.identifyPhase();
+		rateapolicypage.billingSetup();
+		
+		rateapolicypage.coverageDetailsSelect();
+		rateapolicypage.coverageUpdates("Prof Liab-Out", "BINDER-EXCESS");
+	}
+	
+	@AfterMethod
+	public void logoffFromAppclication(ITestResult result) throws IOException, InterruptedException
+	{
+		//homepage.logoutFromeOasis();
+		ExtentReporter.report.endTest(ExtentReporter.logger);
+		
+		if (ITestResult.FAILURE == result.getStatus()) {
+			ExtentReporter.logger.log(LogStatus.FAIL, result.getName());
+			ExtentReporter.logger.log(LogStatus.FAIL, result.getThrowable());
+
+		} else if (ITestResult.SUCCESS == result.getStatus()) {
+			ExtentReporter.logger.log(LogStatus.INFO, "User is logged out of applciation");
+			ExtentReporter.logger.log(LogStatus.PASS, result.getName());
+		} else if (ITestResult.SKIP == result.getStatus()) {
+			ExtentReporter.logger.log(LogStatus.PASS, result.getName());
+		}
+		ExtentReporter.report.flush();
+		Thread.sleep(2000);
+		//driver.close();
 	}
 
 	@AfterClass
 	public void closeBrowser()
-	{
-		driver.quit();
+	{	
+		ExtentReporter.report.close();
+		//driver.quit();
 	}
 }
